e73f1e35b66fde874a2fff1dcb02586bfe3b18c3
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-01-16 22:33:50 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,7 +13,8 @@ def f(a, b, c):
     st.write(valid(a, b, c))
 
 def b(a, b):
-    return None
+  st.balloons
+  st.snow()
 
 number = st.slider(min_value=1, max_value=80, step=1, label="entrez le nombre de recherches a effectué")
 mots = st.text_area("entrez les mots a rechercher")
@@ -22,4 +23,4 @@ ban_words = st.text_area("entrez les mots ne devant pas figurer dans les recherc
 st.button("entrez .", on_click=lambda : f(mots, ban_words, number))
 st.button("quelque chose de fun .", on_click=lambda:st.balloons())
 st.button("autre chose de fun .", on_click=lambda:st.snow())
-st.button("derniere chose de fun .", on_click=lambda:b(st.balloons,st.snow()))
+st.button("derniere chose de fun .", on_click=lambda:b())
