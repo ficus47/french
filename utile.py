43fd73f9a8f2c3ff0ae817206f738d47db15aa70
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-01-16 21:54:34 +0100

    Add files via upload

--- utile.py
+++ utile.py
@@ -1,19 +1,16 @@
-import os
-import tkinter as tk
-
 from googlesearch import search
 
 
-def valid(a,b,c,d,e,f):
-  if c.winfo_ismapped():
+def valid(a,b,d):
+  if True:
     import textwrap
-    d = int(d.get()) if d.get().isdigit() else 7
+    d = int(d) if d.isdigit() else 7
 
 
     text = ""
     text2 = ""
     for i in search(a.get(),lang="fr", num_results=d, advanced=True, sleep_interval=1 if d > 10 else 0):
-      if b.get() not in i.description or b.get() == "" or b.get() == " ":
+      if b not in i.description or b == "" or b == " ":
         text += i.description if i.description != "" or i.description != " " else "aucune description trouvée ;("
         text += "  :  "
         text2 += "~"
@@ -31,9 +28,6 @@ def valid(a,b,c,d,e,f):
 
 
     print(je)
-    e.insert(tk.END, je)
-    c.pack_forget()
-    f.pack()
-    e.pack()
-    os.system("clear")
+    return je
+
 
